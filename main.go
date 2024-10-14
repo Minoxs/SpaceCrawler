11cff0beac7a0b5854a3c452b4ef669282a2afb8
Mino <dev@mino.one>
2024-10-14 01:06:18 -0300

    Clean up CLI program

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/minoxs/DiskExplorer/pkg/DiskExplorer"
 )
@@ -10,44 +13,36 @@ func main() {
 	var dir = DiskExplorer.Map(".")
 	log.Println("\n", dir.String())
 
-	for {
-		dir.Expand()
-		log.Println("Explored")
-		log.Println("\n", dir.String())
-		if dir.IsExplored {
-			break
-		}
-	}
-}
+	var ready = make(chan bool)
+	var cancel = make(chan os.Signal, 1)
+	signal.Notify(cancel, syscall.SIGINT, syscall.SIGTERM)
 
-// func main() {
-// 	var dir = DiskExplorer.Map(".")
-//
-// 	log.Println("\n", dir.String())
-//
-// 	var ready = make(chan bool)
-//
-// 	go func() {
-// 		for {
-// 			dir.Expand()
-// 			log.Println("Exploration done")
-// 			ready <- true
-// 			if dir.FullyExplored() {
-// 				return
-// 			}
-// 		}
-// 	}()
-//
-// 	for {
-// 		// Wait until ready
-// 		<-ready
-// 		log.Println(dir.Name, dir.Size)
-// 		if dir.FullyExplored() {
-// 			log.Println("Done")
-// 			break
-// 		}
-// 	}
-//
-// 	log.SetOutput(os.Stdout)
-// 	log.Println("\n", dir.String())
-// }
+	go func() {
+		var i = 0
+		for {
+			i += 1
+			dir.Expand()
+			log.Println("Exploration done", i)
+			select {
+			case <-cancel:
+				ready <- true
+				return
+			default:
+				if dir.IsExplored {
+					ready <- true
+					return
+				}
+			}
+		}
+	}()
+
+	<-ready
+	if dir.IsExplored {
+		log.Println("Fully explored")
+	} else {
+		log.Println("Partial analysis")
+	}
+
+	log.SetOutput(os.Stdout)
+	log.Println("\n", dir.String())
+}
