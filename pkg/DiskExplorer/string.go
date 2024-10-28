f7fa08c7ba6a5bb146778270c0b8c12e43c4dfeb
Mino <dev@mino.one>
2024-10-28 00:13:29 -0300

    Separate string functions into its own file

---
+++ pkg/DiskExplorer/string.go
@@ -0,0 +1,49 @@
+package DiskExplorer
+
+import (
+	"fmt"
+	"strings"
+)
+
+// Prefix returns a string prefix depending on the node type
+func (d *DiskInfo) Prefix() string {
+	if !d.IsDir {
+		return "F"
+	}
+
+	if d.Explored() {
+		return "E"
+	} else {
+		return "N"
+	}
+}
+
+// FullPrefix returns the Prefix including Depth and Breadth information
+func (d *DiskInfo) FullPrefix() (s string) {
+	if d.IsDir {
+		return d.Prefix() + fmt.Sprintf(" D%d B%d", d.Depth(), d.Breadth())
+	} else {
+		return d.Prefix()
+	}
+}
+
+// String returns a string representation of the node
+func (d *DiskInfo) String() string {
+	return fmt.Sprintf("%s %s %d %s", d.FullPrefix(), d.Mode, d.Size(), d.Name)
+}
+
+// Render will return a string with the file tree representation
+func (d *DiskInfo) Render() string {
+	return d.stringDepth(1)
+}
+
+// stringDepth will recursively build the file tree representation
+func (d *DiskInfo) stringDepth(depth int) (str string) {
+	str = d.String() + "\n"
+
+	for _, child := range d.Children {
+		str += strings.Repeat("\t", depth) + child.stringDepth(depth+1)
+	}
+
+	return
+}
