f085d5f70a48cf702a84d62174762ef2e006c9f6
Mino <dev@mino.one>
2024-11-19 00:01:15 -0300

    Basic updated model

--- cmd/sprawler-tui/main.go
+++ cmd/sprawler-tui/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/rivo/tview"
 
+	"github.com/minoxs/SpaceCrawler/pkg/SpaceCrawler"
 	"github.com/minoxs/SpaceCrawler/pkg/SpaceCrawler/DiskView"
 )
 
@@ -10,13 +11,29 @@ func main() {
 	var app = tview.NewApplication()
 	app.EnableMouse(true).EnablePaste(false)
 
-	var root = DiskView.New(".")
-	app.SetRoot(root.Model(), true)
-	root.OnUpdate = func() {
+	var root = tview.NewFlex().SetDirection(tview.FlexRow)
+	tview.NewGrid()
+	app.SetRoot(root, true)
+
+	var disk = DiskView.New(".")
+	root.AddItem(disk.Model(), 0, 1, true)
+
+	disk.OnUpdate = func() {
 		app.QueueUpdateDraw(func() {})
 	}
+	go disk.Expand()
 
-	go root.Expand()
+	var keys = SpaceCrawler.NewKeyMap().
+		Add(
+			SpaceCrawler.
+				NewKeyBind("Quit", []rune{'q', 'Q'}).WithDescription("Quits Program"),
+		).
+		Add(
+			SpaceCrawler.
+				NewKeyBind("Other", []rune{'o'}).WithDescription("Goes Bazinga"),
+		)
+	var view = SpaceCrawler.NewKeyView(keys)
+	root.AddItem(view.Model(), 2, 1, false)
 
 	if err := app.Run(); err != nil {
 		panic(err)
