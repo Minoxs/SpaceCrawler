d8fd80638d4cfb42cd74a8779b12961c9b6c8ccc
Mino <dev@mino.one>
2025-05-25 14:47:33 -0300

    Add documentation

--- pkg/DiskExplorer/search.go
+++ pkg/DiskExplorer/search.go
@@ -2,6 +2,12 @@ package DiskExplorer
 
 import "context"
 
+// BreadthSearch will iterate over the DiskInfo tree breadth first.
+// This will spawn up to 5000 concurrent go threads to do this search.
+// In other words, this might be a very resource hungry call.
+//
+// This function is NOT thread-safe. Blocks until completion.
+// Cancel the given context to stop the search early.
 func (d *DiskInfo) BreadthSearch(ctx context.Context) {
 	var sem = newSemaphore(5000)
 
@@ -11,6 +17,7 @@ func (d *DiskInfo) BreadthSearch(ctx context.Context) {
 	sem.Wait()
 }
 
+// breadthSearch is an inner helper function to run the search in.
 func (d *DiskInfo) breadthSearch(ctx context.Context, sem *semaphore) {
 	defer sem.Release()
 	if ctx.Err() != nil {
--- pkg/DiskExplorer/sync.go
+++ pkg/DiskExplorer/sync.go
@@ -4,24 +4,29 @@ import (
 	"time"
 )
 
+// semaphore ensures only N threads can access some resource
 type semaphore struct {
 	forks chan struct{}
 }
 
+// newSemaphore creates a semaphore with max resources
 func newSemaphore(max int) *semaphore {
 	return &semaphore{
 		forks: make(chan struct{}, max),
 	}
 }
 
+// Acquire enters the semaphore
 func (s *semaphore) Acquire() {
 	s.forks <- struct{}{}
 }
 
+// Release releases the semaphore
 func (s *semaphore) Release() {
 	<-s.forks
 }
 
+// Wait will block until the semaphore count reaches 0
 func (s *semaphore) Wait() {
 	for len(s.forks) > 0 {
 		time.Sleep(100 * time.Millisecond)
